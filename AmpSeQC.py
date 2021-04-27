409a6990ffa1a5d992652240a3f0878f3ef9abf8
tstraub <tstraub@broadinstitute.org>
2021-04-27 14:15:36 -0400

    Add epilog

--- AmpSeQC.py
+++ AmpSeQC.py
@@ -377,7 +377,9 @@ def main():
     global DEFAULT_ANNOT
     DEFAULT_REF = os.path.realpath(os.path.join(os.path.dirname(__file__), DEFAULT_REF))
     DEFAULT_ANNOT = os.path.realpath(os.path.join(os.path.dirname(__file__), DEFAULT_ANNOT))
-    parser = ArgumentParser(description="Amplicon sequencing quality control pipeline", usage="%(prog)s [options] -c [read_counts] -r [ref.fasta] -a [annot.gff] fastq [fastq ...]")
+    parser = ArgumentParser(description="Amplicon sequencing quality control pipeline", 
+                            usage="%(prog)s [options] -c [read_counts] -r [ref.fasta] -a [annot.gff] fastq [fastq ...]",
+                            epilog="Contact tim.straub@broadinstitute.org for details.")
     parser.add_argument("fastq", nargs="+", help="Paired-end fastq files (can be gzipped). Expects pairs to be in separate files.")
     parser.add_argument("-c", "--counts", default="read_counts.tsv", help="Read count tsv file (default: read_counts.tsv)")
     parser.add_argument("-r", "--ref", default=DEFAULT_REF, help="Indexed reference fasta file to align to (default: %s)" % DEFAULT_REF)
